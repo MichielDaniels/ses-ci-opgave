57f9af178d7ea167d7243abe45dca669685b83f6
MichielDaniels <60984480+MichielDaniels@users.noreply.github.com>
2020-04-28 17:11:14 +0200

    calculator implemented with words as operands

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -1,6 +1,26 @@
 public class calculator {
     public static void main(String[] args) {
-        // dit is een test
-        System.out.println("dit is een geslaagde test");
+        int x = Integer.parseInt(args[0]);
+        String bewerking = args[1];
+        int y = Integer.parseInt(args[2]);
+
+        int uitkomst = rekenmachine(x, y, bewerking);
+        System.out.println(uitkomst);
+    }
+
+    public static int rekenmachine(int x, int y, String bewerking){
+        int z; // uitkomst
+        if (bewerking.equals("plus")) {
+            z = x+y;
+        } else if (bewerking.equals("min")){
+            z = x-y;
+        } else if (bewerking.equals("maal")){
+            z = x*y;
+        } else if (bewerking.equals("delen")){
+            z = x/y;
+        } else{
+            throw new java.lang.Error("Operator not recognized");
+        }
+        return z;
     }
 }
