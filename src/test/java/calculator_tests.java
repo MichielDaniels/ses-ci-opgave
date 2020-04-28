51e9edea9cabacaf25bd7152b8ec11e114573ae5
MichielDaniels <60984480+MichielDaniels@users.noreply.github.com>
2020-04-28 18:54:30 +0200

    tests added

---
+++ src/test/java/calculator_tests.java
@@ -0,0 +1,48 @@
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class calculator_tests {
+
+    @Test
+    public void rekenmachine_plus(){
+        // 1. Arrange
+        var calc = new calculator();
+        // 2. Act
+        var resultaat = calc.rekenmachine(4, 1, "plus");
+        // 3. Assert
+        assertThat(resultaat, is(5));
+    }
+
+    @Test
+    public void rekenmachine_min(){
+        // 1. Arrange
+        var calc = new calculator();
+        // 2. Act
+        var resultaat = calc.rekenmachine(4, 1, "min");
+        // 3. Assert
+        assertThat(resultaat, is(3));
+    }
+
+    @Test
+    public void rekenmachine_maal(){
+        // 1. Arrange
+        var calc = new calculator();
+        // 2. Act
+        var resultaat = calc.rekenmachine(4, 2, "maal");
+        // 3. Assert
+        assertThat(resultaat, is(8));
+    }
+
+    @Test
+    public void rekenmachine_delen(){
+        // 1. Arrange
+        var calc = new calculator();
+        // 2. Act
+        var resultaat = calc.rekenmachine(4, 2, "delen");
+        // 3. Assert
+        assertThat(resultaat, is(2));
+    }
+    
+}
